6fb7f9c3a5e621aa9679148b90f7a2e1d4b2bec2
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2019-10-27 22:21:44 -0400

    added more tests

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest.h"
-#include "funcs.h"
+      #include <iostream>
+                  #define DOCTEST_CONFIG_IMPLEMENT
+                        #include "doctest.h"
+               #include "funcs.h"
 
-int main(int argc, char *argv[])
-{
+                  int main(int argc, char *argv[])
+               {
   // your code here
-  return 0;
-}
+   return 0;
+   }
--- tests.cpp
+++ tests.cpp
@@ -9,3 +9,7 @@
 TEST_CASE("Testing bad-code.cpp"){
    CHECK(indent("bad-code.cpp")=="int main(){\n\t// Hi, I'm a program!\n\tint x = 1;\n\tfor(int i = 0; i < 10; i++) {\n\t\tcout << i;\n\t\tcout << endl;\n\t}\n}\n");
 }
+
+TEST_CASE("Testing main.cpp"){
+   CHECK(indent("main.cpp")=="#include <iostream>\n#define DOCTEST_CONFIG_IMPLEMENT\n#include \"doctest.h\"\n#include \"funcs.h\"\n\nint main(int argc, char *argv[])\n{\n\t// your code here\n\treturn 0;\n}\n");
+}
